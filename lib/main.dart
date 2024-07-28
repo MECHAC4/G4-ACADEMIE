0791e3c8c6762efba41651d9ecc14502c81add5d
MECHAC4 <josesecurise@gmail.com>
2024-07-28 13:33:51 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:g4_academie/screens/auth_screen/welcome_screen.dart';
 import 'package:g4_academie/theme/theme.dart';
 
 import 'constants.dart';
+import 'firebase_options.dart';
 
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
